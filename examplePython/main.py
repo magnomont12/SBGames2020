33b9054f9ebff2932f61508ce240ebe6ef9c1081
Alexandre Magno <magnomont12@gmail.com>
2020-03-09 19:16:25 -0300

    Pode ser visto o erro de conversão por possiveis problemas de ref

--- examplePython/main.py
+++ examplePython/main.py
@@ -12,21 +12,15 @@ import player
 import population
 
 jogador = player.Player(20,500,5,0)
-pop = population.Population(13,0.1,10,jogador)
+pop = population.Population(13,0.1,100,jogador)
 
 cont = 0
 while(pop.minFitness()[0] > 1):
     pop.calcFitness(jogador)
     pop.naturalSelection()
     pop.generate()
-    print("Entrei no laço")
-    print(pop.minFitness()[0])
-    print(pop.minFitness()[1])
-    cont+=1
     for a in pop.population:
         print(a.fitness, a.dna, jogador.calcBatalha(a)[0])
-    if cont > 3:
-        break
 
 
 
